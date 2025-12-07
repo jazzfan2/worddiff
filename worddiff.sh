f990321531802feaa7422a552b40fc7729787658
jazzfan2 <149713903+jazzfan2@users.noreply.github.com>
2025-12-08 00:12:17 +0100

    First commit

--- worddiff.sh
+++ worddiff.sh
@@ -70,3 +70,22 @@ sed -E "s/(([^ 	]+[ 	]+){$wordcount})/\1\n/g" |
 
 # De spatie in de '<span style=xxx>'-tag weer terugbrengen, en het resultaat wegschrijven naar een .html-file:
 sed 's/<span_style=/<span style=/g'    >| out.html
+
+#####################################################################################
+#
+# Copyright (C) 2025 Rob Toscani <rob_toscani@yahoo.com>
+#
+# worddiff.sh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# worddiff.sh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+######################################################################################
